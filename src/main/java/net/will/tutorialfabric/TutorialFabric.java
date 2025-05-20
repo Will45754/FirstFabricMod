8a698bef5c99b0400af338b125a61abb3cfe81e2
will45754 <strommerwilliam5@gmail.com>
2025-05-20 13:08:26 -0600

    mit

---
+++ src/main/java/net/will/tutorialfabric/TutorialFabric.java
@@ -0,0 +1,16 @@
+package net.will.tutorialfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialFabric implements ModInitializer {
+	public static final String MOD_ID = "tutorialfabric";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
